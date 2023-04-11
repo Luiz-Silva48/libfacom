b6c645ab12ad4f46adebe031c545ba87243b9d85
Luiz-Silva48 <112423462+Luiz-Silva48@users.noreply.github.com>
2023-04-11 14:02:21 -0400

    Update heap.c

--- src/heap.c
+++ src/heap.c
@@ -12,3 +12,47 @@ int heap_filho_esq(int pai){
 int heap_filho_dir(int pai){
     return pai*2 + 2;
 }
+
+void troca(int *v1, int *v2){
+    int aux = *v1;
+    *v1 = *v2;
+    *v2 = aux;
+}
+void heap_desce(int v[], int n, int pai){
+    int e,d,maior;
+
+    e = heap_filho_esq(pai);
+    d = heap_filho_dir(pai);
+
+    maior = pai;
+    if ( e< n && v[e] > v[pai]){
+        maior = e;
+    }
+    if ( d < n && v[d] > v[maior]){
+        maior = d;
+    }
+    if (maior != pai){
+        troca(&v[pai],&v[maior]);
+        heap_desce(v,n,maior);
+    }
+
+}
+
+
+void heap_constroi(int v[],int n){
+    int i;
+    int ultimo = n-1;
+    for (i = heap_pai(ultimo);i>=0;i--){
+        heap_desce(v,n,i);
+    }
+}
+
+
+int  heap_extrai_max(int v[],int *n){
+    int max = v[0];
+    int ultimo = *n -1;
+    troca(&v[0],&v[ultimo]);
+    heap_desce(v,ultimo,0);
+    *n = *n-1;
+    return max;
+}
