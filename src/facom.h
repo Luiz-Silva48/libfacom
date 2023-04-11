b310ce8a5b198c2030c049a14cdc2a42a0eb6144
Luiz-Silva48 <112423462+Luiz-Silva48@users.noreply.github.com>
2023-04-11 15:55:52 -0400

    Update facom.h

--- src/facom.h
+++ src/facom.h
@@ -1,12 +1,14 @@
-#ifndef _UTILS_
-#define _UTILS_
+# ifndef _UTILS_
+# define  _UTILS_
 
-int heap_pai(int filho);
-int heap_filho_esq(int pai);
-int heap_filho_dir(int pai);
-void heap_desce(int v[], int n, int pai);
-void heap_constroi(int v[],int n);
-int  heap_extrai_max(int v[],int *n);
-int e_par(int v);
+int  heap_pai ( int filho);
+int  heap_filho_esq ( int pai);
+int  heap_filho_dir ( int pai);
+void  heap_desce ( int v[], int n, int pai);
+void  heap_constroi ( int v[], int n);
+int   heap_extrai_max ( int v[], int *n);
+void  heap_sort ( int v[], int n);
+void  heap_altera_prioridade ( int v[], int n, int pos, int novo_valor);
+int  e_par ( int v);
 
-#endif 
+#endif _ 
